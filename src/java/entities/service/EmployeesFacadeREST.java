a9c158ccd2a0b1acab23866387243408a9322b44
gwq2427527 <guoweiqi242@outlook.com>
2016-10-28 23:25:36 +0800

    Update EmployeesFacadeREST.java

--- src/java/entities/service/EmployeesFacadeREST.java
+++ src/java/entities/service/EmployeesFacadeREST.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -69,6 +70,61 @@ public class EmployeesFacadeREST extends AbstractFacade<Employees> {
         return super.findAll();
     }
 
+    
+    @GET
+    @Path("/firstname/{firstname}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List <Employees> findByEmpFtname(@PathParam("firstname") String firstname) {
+        Query query = em.createNamedQuery("Employees.findByEmpFirstname").setParameter("empFirstname", firstname);
+        return query.getResultList();
+    }
+    
+    @GET
+    @Path("/lastname/{lastname}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List <Employees> findByEmpLastname(@PathParam("lastname") String lastname) {
+        Query query = em.createNamedQuery("Employees.findByEmpLastname").setParameter("empLastname", lastname);
+        return query.getResultList();
+    }
+    
+    @GET
+    @Path("/username/{username}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List <Employees> findByEmpUsername(@PathParam("username") String username) {
+        Query query = em.createNamedQuery("Employees.findByEmpUsername").setParameter("empUsername", username);
+        return query.getResultList();
+    }
+    @GET
+    @Path("/password/{password}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List <Employees> findByEmpPassword(@PathParam("password") String password) {
+        Query query = em.createNamedQuery("Employees.findByEmpPassword").setParameter("empPassword", password);
+        return query.getResultList();
+    }
+    
+        @GET
+    @Path("/email/{email}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List <Employees> findByEmpEmail(@PathParam("email") String email) {
+        Query query = em.createNamedQuery("Employees.findByEmpEmail").setParameter("empEmail", email);
+        return query.getResultList();
+    }
+    
+    @GET
+    @Path("/phone/{phone}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List <Employees> findByEmpPhone(@PathParam("phone") String phone) {
+        Query query = em.createNamedQuery("Employees.findByEmpPhone").setParameter("empPhone", phone);
+        return query.getResultList();
+    }
+    @GET
+    @Path("/registered/{registered}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List <Employees> findByEmpRegistered(@PathParam("registered") boolean registered) {
+        Query query = em.createNamedQuery("findByEmpRegistered.findByEmpPassword").setParameter("empRegistered", registered);
+        return query.getResultList();
+    }
+    
     @GET
     @Path("{from}/{to}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
